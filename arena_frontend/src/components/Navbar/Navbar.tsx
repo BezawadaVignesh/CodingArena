f09ba313579f8925b8a71477bd76b557a3952301
Rahul <krahulyavapuram@gmail.com>
2024-11-11 00:07:19 +0530

    responsive Navbar

--- arena_frontend/src/components/Navbar/Navbar.tsx
+++ arena_frontend/src/components/Navbar/Navbar.tsx
@@ -1,14 +1,22 @@
 import { Logout } from "@mui/icons-material";
 import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
+import MenuIcon from '@mui/icons-material/Menu';
 import {
   Avatar,
   Box,
   Button,
   Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
   ListItemIcon,
+  ListItemText,
   Menu,
   MenuItem,
   styled,
+  useTheme
 } from "@mui/material";
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -78,12 +86,38 @@ function UserDisplay({ user, logOut }: { user: string; logOut: () => void }) {
   );
 }
 
+const NavButtonActive = styled(Button)(({ theme }) => ({
+  textTransform: 'none', // Remove uppercase transformation
+  color: "var(--text-color)", // Use the primary text color
+  position: 'relative',
+  paddingInline: '10px',
+  fontWeight: 'bold',
+  "&:hover": {
+    backgroundColor: "transparent",
+  },
+  '&::after': {
+    content: '""',
+    position: 'absolute',
+    width: '95%',
+    height: '2px',
+    display: 'block',
+    marginTop: '20px',
+    borderRadius: '.25rem',
+    // left: '0',
+    background: "var(--text-color)",
+    transition: 'width 0.3s ease',
+  },
+}));
+
 const NavButton = styled(Button)(({ theme }) => ({
   textTransform: "none",
   color: "var(--text-color)",
   position: "relative",
   fontSize: "0.95rem",
   paddingInline: "10px",
+  "&:hover": {
+    backgroundColor: "transparent",
+  },
   "&::after": {
     content: '""',
     position: "absolute",
@@ -110,46 +144,116 @@ const NavBox = styled(Box)(({ theme }) => ({
   position: "fixed",
   paddingBlock: "5px",
   width: "100%",
-  
+
   top: 0,
   zIndex: 100,
 }));
 
+
+import CloseIcon from '@mui/icons-material/Close';
+import CodeIcon from '@mui/icons-material/Code';
+import CollectionsIcon from '@mui/icons-material/Collections';
+import ContactMailIcon from '@mui/icons-material/ContactMail';
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
 const Navbar = () => {
-  const pages = ["Anouncements", "Contests"];
-  const move = ["/tt#asdfghjkl", "/contests"] as string[];
+  const pages = ["Home", "About Us", "Anouncements", "Projects", "Gallery", "Contact Us"];
+  const pageIcons = [<HomeIcon />, <InfoIcon />, <NotificationsIcon />, <CodeIcon />, <CollectionsIcon />, <ContactMailIcon />]
+  const move = ["/home", "/aboutus", "/announcements", "/projects", "/gallery", '/contactus'] as string[];
   const navigate = useNavigate();
   const location = useLocation();
+  const theme = useTheme();
+  const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
+  const linkIdx = move.findIndex((path) => location.pathname == path)
   // const { user, logOut } = useAuth() || { user: undefined };
-
+  const isActive = (path: string) => location.pathname === path;
   return (
-    <NavBox>
-      <motion.div
-      initial={{ y: '-110%' }}
-      style={{display: "flex",
-        alignItems: "center",
-        justifyContent: "space-around",}}
-      animate={
-        { y: 0 }
-      }
-      transition={{ duration: 0.5 , ease: "easeOut", delay: 2.5 }}
-    >
-      {/* <nav className={""} style={{}}> */}
+    <div>
+      <NavBox>
+        <motion.div
+          initial={{ y: '-110%' }}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-around",
+          }}
+          animate={
+            { y: 0 }
+          }
+          transition={{ duration: 0.5, ease: "easeOut", delay: 2.5 }}
+        >
+          {/* <nav className={""} style={{}}> */}
 
-      {/* {user ? <UserDisplay user={user} logOut={logOut} /> : location.pathname == '/login' ? <></> :<Link to="/login">Login</Link>} */}
-      <div className="__name-logo">Coding Club</div>
-      {/* <Logo /> */}
-      {/* <img src={Logo} width={100}/> */}
-      {
-        <div className={"__nav-buttons "+style.nav}>
-          {pages.map((page, index) => (
-            <NavButton onClick={() => navigate(move[index])}>{page}</NavButton>
-          ))}
-        </div>
-      }
-      </motion.div>
-      {/* </nav> */}
-    </NavBox>
+          {/* {user ? <UserDisplay user={user} logOut={logOut} /> : location.pathname == '/login' ? <></> :<Link to="/login">Login</Link>} */}
+
+          <div className="__menu-bar" >
+
+            <IconButton
+              aria-label="open drawer"
+              onClick={() => { setDrawerOpen(true) }}
+            >
+
+              <MenuIcon sx={{color: "black"}} />
+            </IconButton>
+          </div>
+          <div className="__name-logo">Coding Club</div>
+
+
+          {/* <Logo /> */}
+          {/* <img src={Logo} width={100}/> */}
+          {
+            <div className={"__nav-buttons " + style.nav} >
+              {pages.map((page, index) => (
+                (index == linkIdx) ?<NavButtonActive>{page}</NavButtonActive>: <NavButton sx={{
+                  fontWeight: isActive(move[index]) ? "bold" : "normal", 
+                  fontSize: isActive(move[index]) ? 20: ""
+                }} onClick={() => navigate(move[index])}>{page}</NavButton>
+              ))}
+            </div>
+
+          }
+        </motion.div>
+        {/* </nav> */}
+      </NavBox>
+
+      <Drawer open={drawerOpen} transitionDuration={1000} onClose={() => { setDrawerOpen(false); }}>
+        <motion.div
+          initial={{ x: '-110%' }}
+          animate={
+            { x: 0 }
+          }
+          transition={{ duration: 0.5, ease: "easeOut", delay: 0.2 }}
+        >
+          <Box sx={{ width: "100vh", }} role="presentation">
+            <List>
+              <ListItem>
+                <CloseIcon style={{ fontSize: "2em" }} onClick={() => { setDrawerOpen(false); }} />
+              </ListItem>
+              <ListItem>
+
+                <div className="__name-logo" style={{ color: theme.palette.mode == "dark"? "white": "" }}>Coding Club</div>
+
+              </ListItem>
+              <Divider />
+              {pages.map((text, index) => (
+                
+                <ListItem key={text} disablePadding sx={{ backgroundColor: isActive(move[index]) ? theme.palette.mode == "dark" ? "white": "black": "", color: isActive(move[index]) ? theme.palette.mode == "dark" ? "black": "white": "" }}>
+                  <ListItemButton onClick={() => { setDrawerOpen(false);  navigate(move[index]) }} >
+                    <ListItemIcon sx={{color: isActive(move[index]) ? theme.palette.mode == "dark" ? "black": "white": "" }}>
+                      {pageIcons[index]}
+                    </ListItemIcon>
+                    <ListItemText primary={text} sx={{ fontSize: "5em" }} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+
+          </Box>
+        </motion.div>
+      </Drawer>
+    </div>
   );
 };
 
